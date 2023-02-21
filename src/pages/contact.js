9a85e4725eea7fdde498ccf6078fe86fc23b9def
Marlon Couto <devpatola@proton.me>
2023-02-21 16:03:56 -0300

    Adiciona o componente Contact e suas meta tags

---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Head from "next/head";
+
+export default function Contact() {
+  return (
+    <>
+      <Head>
+        <title>Página de Contato</title>
+      </Head>
+      <h1>Página de Contato</h1>
+    </>
+  );
+}
