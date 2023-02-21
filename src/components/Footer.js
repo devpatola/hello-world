2ef94a6eb7dcb2d84eeb8a47e65c7f7d9497e6de
Marlon Couto <devpatola@proton.me>
2023-02-21 02:49:57 -0300

    Adiciona componente MainContaine

---
+++ src/components/Footer.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Footer() {
+  return <footer>Curso de Next.js &copy; 2023</footer>;
+}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,16 +3,18 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <ul>
-      <li>
-        <Link href="/">Home</Link>
-      </li>
-      <li>
-        <Link href="/products">Produtos</Link>
-      </li>
-      <li>
-        <Link href="/about">Sobre</Link>
-      </li>
-    </ul>
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/products">Produtos</Link>
+        </li>
+        <li>
+          <Link href="/about">Sobre</Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
